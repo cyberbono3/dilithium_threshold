e1ad3c75b1f31c61023e0a69e47ebe5b05744585
Andrei Ivasko <cyberbono3@gmail.com>
2025-11-16 15:25:57 +0300

    wip

--- core/src/dilithium/mod.rs
+++ core/src/dilithium/mod.rs
@@ -1,6 +1,12 @@
+//! Core Dilithium building blocks: parameters, utilities, and threshold tooling.
+
 pub mod error;
 pub mod params;
 pub mod shamir;
-pub mod sign;
 pub mod threshold;
 pub mod utils;
+
+pub use error::{DilithiumError, DilithiumResult};
+pub use threshold::{
+    ThresholdError, ThresholdInfo, ThresholdKeyShare, ThresholdSignature,
+};
